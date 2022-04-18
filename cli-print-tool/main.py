7ec81e5dad088fe5f3347a2b2115f7caab8c1ffa
Julien Widmer <ju.widmer@pm.me>
2022-04-18 18:00:35 -0400

    Added demo code for CLIPrintTool

---
+++ cli-print-tool/main.py
@@ -0,0 +1,19 @@
+from cli_print_tool import CLIPrintTool  # 1) import the class
+
+if __name__ == '__main__':
+    # 2) define the maximum line length for the content
+    # i.e. 100 characters
+    cli_print_tool = CLIPrintTool(80)
+
+    # 3) use the CLIPrintTool methods to display content to the user
+    # i.e. display a title inside a box
+    cli_print_tool.textbox("Hello World!")
+
+    # i.e. display a long text inside a box
+    paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent venenatis feugiat auctor. Praesent" \
+                " aliquet enim volutpat felis sagittis facilisis. Proin vitae pulvinar sapien. Morbi leo nunc, " \
+                "commodo sed hendrerit nec, pretium sed ligula. Pellentesque sit amet vulputate nibh. Fusce iaculis " \
+                "elementum feugiat. Curabitur dignissim, magna id suscipit mattis, ante urna consectetur justo, vel " \
+                "pretium est nulla at augue. Aliquam rhoncus ullamcorper mauris, lacinia luctus urna interdum ac. " \
+                "Donec sollicitudin turpis eu eleifend tristique."
+    cli_print_tool.textbox(paragraph)
