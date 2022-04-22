1bf37da28dc3343c94a97c381a6010f7e1025e56
Julien Widmer <ju.widmer@pm.me>
2022-04-22 11:19:10 -0400

    Added example for long texts

--- cli-print-tool/example.py
+++ cli-print-tool/example.py
@@ -13,13 +13,30 @@ print()  # blank line
 
 # display a list
 elements = [
-    "Display text inside a box",
-    "Display headings (text with a title and subtitle",
+    "Display and automatically perform line-wrapping for long texts",
+    "Display texts inside boxes",
+    "Display headings (texts with a title and subtitle)",
     "Display elements as a numbered list with or without title",
-    "Change the text alignment for any of these features"
+    "Change text alignment (supported by any of these features)"
 ]
 
 CLIPrintTool().list(elements, "What can I do with CLIPrintTool?")
+print()  # blank line
+
+# display long text without borders
+long_text = """CLIPrintTool will automatically wrap your text to respect the maximum line length (by default 100 \
+characters) and preserve line returns.
+
+The text below is a complete \"Lorem Ipsum\" paragraph:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis sollicitudin ante. Etiam ultricies cursus ligula, \
+et vulputate tortor elementum sed. Nunc et ante interdum, imperdiet turpis sed, egestas sem. Nunc ipsum risus, rutrum \
+a sodales a, mattis quis nunc. Nunc placerat leo elit, at iaculis nulla porta a. Sed condimentum ante at suscipit \
+facilisis. Nulla et iaculis augue. Vivamus consequat metus odio, ac fringilla justo porttitor a. In nisi velit, \
+vulputate eu consequat sed, pellentesque nec est. Nulla facilisi. Pellentesque semper diam ac tempor finibus. In id \
+pulvinar risus. Morbi placerat tellus et vehicula rutrum. Nulla euismod scelerisque est."""
+
+CLIPrintTool().text(long_text)
 print()  # blank line
 
 # display heading
